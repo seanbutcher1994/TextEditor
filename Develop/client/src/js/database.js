6f8442c550635f638f5a9eebb40a721b3d2fd61a
seanbutcher1994 <110334815+seanbutcher1994@users.noreply.github.com>
2023-01-02 11:59:21 +0800

    database.js put request

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,7 +13,20 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to database');
+  // connect to DB and version to use
+  const jateDb = await openDB('jate', 1);
+  // create new transaction. Specifies which DB to post to and what priveleges.
+  const tx = jateDb.transaction('jate', 'readwrite');
+  // open the object store
+  const objStore = tx.objectStore('jate');
+  // pass in content
+  const req = objStore.put({ id: id, value: value })
+  // confirm data added 
+  const res = await req;
+  console.log('data saved to database', res);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
